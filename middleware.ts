64962c4a5702d43ccd737ed0c9227776ad1ccfce
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-10-11 23:28:12 +0545

    routes

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,48 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
-  "/signin",
-  "/signup",
-  "/",
-  "home"
+    "/sign-in",
+    "/sign-up",
+    "/",
+    "/home"
 ])
 const isPublicApiRoute = createRouteMatcher([
-  "/api/videos"
+    "/api/videos"
 ])
+
+
 export default clerkMiddleware((auth, req) => {
-  const {userId} = auth();
-  const currentUrl = new URL(req.url)
-  const isAccessingDashboard = currentUrl.pathname === "/home"
-  const  isApiRequest =  currentUrl.pathname.startsWith("/api")
+    const {userId} = auth();
+    const currentUrl = new URL(req.url)
+     const isAccessingDashboard = currentUrl.pathname === "/home"
+     const isApiRequest = currentUrl.pathname.startsWith("/api")
 
-  if(userId && isPublicRoute(req) && !isAccessingDashboard){
-     return NextResponse.redirect(new URL("/home", req.url))
-  }
-
-  if(!userId){
-    if(!isPublicRoute(req) && !isPublicApiRoute(req)){
-        return NextResponse.redirect(new URL("/signin", req.url))
+     // If user is logged in and accessing a public route but not the dashboard
+    if(userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL("/home", req.url))
     }
-    if(isApiRequest && !isPublicApiRoute(req)){
-      return NextResponse.redirect(new URL("/signin", req.url))
-    }
-  }
+    //not logged in
+    if(!userId){
+        // If user is not logged in and trying to access a protected route
+        if(!isPublicRoute(req) && !isPublicApiRoute(req) ){
+            return NextResponse.redirect(new URL("/sign-in", req.url))
+        }
 
-  return NextResponse.next()
+        // If the request is for a protected API and the user is not logged in
+        if(isApiRequest && !isPublicApiRoute(req)){
+            return NextResponse.redirect(new URL("/sign-in", req.url))
+        }
+    }
+    return NextResponse.next()
 
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+    matcher: [
+      // Skip Next.js internals and all static files, unless found in search params
+      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+      // Always run for API routes
+      '/(api|trpc)(.*)',
+    ],
+  };

ebe0b9d514b0ec0631a761c2afdff113567aa2a4
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-10-12 20:31:21 +0545

    Prisma with NeoDB

--- middleware.ts
+++ middleware.ts
@@ -13,24 +13,24 @@ const isPublicApiRoute = createRouteMatcher([
 
 
 export default clerkMiddleware((auth, req) => {
-    const {userId} = auth();
+    const { userId } = auth();
     const currentUrl = new URL(req.url)
-     const isAccessingDashboard = currentUrl.pathname === "/home"
-     const isApiRequest = currentUrl.pathname.startsWith("/api")
+    const isAccessingDashboard = currentUrl.pathname === "/home"
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
 
-     // If user is logged in and accessing a public route but not the dashboard
-    if(userId && isPublicRoute(req) && !isAccessingDashboard) {
+    // If user is logged in and accessing a public route but not the dashboard
+    if (userId && isPublicRoute(req) && !isAccessingDashboard) {
         return NextResponse.redirect(new URL("/home", req.url))
     }
     //not logged in
-    if(!userId){
+    if (!userId) {
         // If user is not logged in and trying to access a protected route
-        if(!isPublicRoute(req) && !isPublicApiRoute(req) ){
+        if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
             return NextResponse.redirect(new URL("/sign-in", req.url))
         }
 
         // If the request is for a protected API and the user is not logged in
-        if(isApiRequest && !isPublicApiRoute(req)){
+        if (isApiRequest && !isPublicApiRoute(req)) {
             return NextResponse.redirect(new URL("/sign-in", req.url))
         }
     }
@@ -40,9 +40,9 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
     matcher: [
-      // Skip Next.js internals and all static files, unless found in search params
-      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      // Always run for API routes
-      '/(api|trpc)(.*)',
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
     ],
-  };
+};
